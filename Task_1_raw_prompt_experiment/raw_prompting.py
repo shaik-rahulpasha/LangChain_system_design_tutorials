2d0b58ec11598fe7e47de331a71343232e1601bf
Shaik Rahulpasha <shaikrahulpasha00@gmail.com>
2025-12-15 14:34:25 +0530

    added tool calling

--- Task_1_raw_prompt_experiment/raw_prompting.py
+++ Task_1_raw_prompt_experiment/raw_prompting.py
@@ -3,7 +3,7 @@ warnings.filterwarnings("ignore")
 import os 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-
+from langchain.tools import tool
 load_dotenv()
 
 model=ChatGoogleGenerativeAI(
@@ -81,3 +81,66 @@ for response in model.batch_as_completed([
     print(response)
 """
 
+
+########################--------------------Tool callings -----------------------------######################################
+
+
+"""
+@tool
+def get_weather(location: str) -> str : 
+    this function return the current weather of location
+    Arguments : 
+        1. Takes str as input for location
+    
+    return "its sunny in {location}"
+
+@tool 
+def description_about_city(Location: str)-> str: 
+     This function descipts the city nature and details and tourist places and food items.
+     Args: 
+          1. Takes location as input 
+     
+    return f"city {Location} has beautiful raw natured ness "
+
+
+model_with_tools=model.bind_tools([get_weather, description_about_city])
+
+model_with_tools=model.bind_tools([get_weather, description_about_city],tool_choice="any")
+
+response=model_with_tools.invoke("get description nature in Pune")
+
+for tool_call in response.tool_calls:
+    print(f"Tool : {tool_call['name']}")
+    print(f"tool args ; {tool_call['args']}")
+
+    
+
+    for chunk in model_with_tools.stream(
+    "What's the weather in Boston and Tokyo?"
+):
+    # Tool call chunks arrive progressively
+    for tool_chunk in chunk.tool_call_chunks:
+        if name := tool_chunk.get("name"):
+            print(f"Tool: {name}")
+        if id_ := tool_chunk.get("id"):
+            print(f"ID: {id_}")
+        if args := tool_chunk.get("args"):
+            print(f"Args: {args}")
+
+# Output:
+# Tool: get_weather
+# ID: call_SvMlU1TVIZugrFLckFE2ceRE
+# Args: {"lo
+# Args: catio
+# Args: n": "B
+# Args: osto
+# Args: n"}
+# Tool: get_weather
+# ID: call_QMZdy6qInx13oWKE7KhuhOLR
+# Args: {"lo
+# Args: catio
+# Args: n": "T
+# Args: okyo
+# Args: "}
+
+"""
